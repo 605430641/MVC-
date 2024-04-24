ed89f0962aa7ea02ae51701e590222a98e087a56
泽西 <37681153+605430641@users.noreply.github.com>
2024-04-24 18:10:07 +0800

    feat:增加日志组件扩展

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,8 +1,30 @@
+#region 创建Builder
+//GOF23种设计模式之创建型模式之建造者模式 作用：将复杂对象的构造和它的表示分离,使同样的构建过程 可以创建不同的表示
+//将一个复杂对象分解多个简单对象 然后一步步构建 相当于产品的组成部分不变 但是每一部分可以灵活选择
+//里面创建的各种 webapplication和webHostBuilder,configBuilder 都是为了创建web程序的初始化
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+// Add services to the container.视图的builder
 builder.Services.AddControllersWithViews();
 
+
+#endregion
+
+#region 配置文件扩展
+
+
+
+#endregion
+//如果加载两个配置文件 有相同的配置 取最后builder的
+//builder.Configuration.AddJsonFile("appsettings.Demo.Json",true,false);
+
+#region 日志组件扩展
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole().AddDebug();
+builder.Logging.AddLog4Net();
+
+
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +36,9 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+#region Use中间件：管道默许配置
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -21,9 +46,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
+#endregion
+
 app.MapControllerRoute(
                        name: "default",
                        pattern: "{controller=Home}/{action=Index}/{id?}"
                       );
-
+ //启动坚挺——Kestrel
 app.Run();
