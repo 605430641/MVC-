715bbd72f157930baac6919f4432fcd8210c5954
泽西 <37681153+605430641@users.noreply.github.com>
2024-04-28 20:07:52 +0800

    补充提交

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,3 +1,11 @@
+
+//builder.Configuration 读取配置文件帮助类 可以配置多个数据源
+//builder.logging:配置下项目的日志组件 常用的log4net
+//builder.setvice是ioc注册
+
+//builder.Host
+
+
 #region 创建Builder
 //GOF23种设计模式之创建型模式之建造者模式 作用：将复杂对象的构造和它的表示分离,使同样的构建过程 可以创建不同的表示
 //将一个复杂对象分解多个简单对象 然后一步步构建 相当于产品的组成部分不变 但是每一部分可以灵活选择
@@ -9,11 +17,7 @@ builder.Services.AddControllersWithViews();
 
 #endregion
 
-#region 配置文件扩展
 
-
-
-#endregion
 //如果加载两个配置文件 有相同的配置 取最后builder的
 //builder.Configuration.AddJsonFile("appsettings.Demo.Json",true,false);
 
@@ -21,10 +25,13 @@ builder.Services.AddControllersWithViews();
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole().AddDebug();
-builder.Logging.AddLog4Net();
-
+// builder.Logging.AddLog4Net();      、、log4组件 需要添加log4net.config 暂时没有添加
+ // builder.Logging.AddFilter("System",   LogLevel.Debug); //常用的日志组件
+ // builder.Logging.AddFilter("Microsoft",LogLevel.Warning); //常用的日志组件
 
 #endregion
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
